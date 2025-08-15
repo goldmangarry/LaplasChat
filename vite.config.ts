425a50088a8a81934790b47b35fa85dd47481df4
vladilsav.khakimov <perezzzq@yandex.ru>
2025-08-15 13:58:20 +0300

    change backend url

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "https://dev-apilaplas-backend.onrender.com",
+				target: "https://main-apilaplas-backend.onrender.com",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 				secure: false,
